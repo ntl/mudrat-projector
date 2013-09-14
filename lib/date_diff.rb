2a98d7e12812c866eafb8002b7ccf21ffb291715
ntl <nathanladd+github@gmail.com>
2013-09-14 13:27:30 -0500

    Refactor DateDiff for DRYness

---
+++ lib/date_diff.rb
@@ -0,0 +1,138 @@
+module DateDiff
+  extend self
+
+  def date_diff *maybe_unit_from_to, unit: nil, from: nil, to: nil
+    if [unit, from, to].all? &:nil?
+      unit, from, to = maybe_unit_from_to
+    end
+    fetch_subclass(unit).new(from, to).calculate
+  end
+
+  def fetch_subclass unit
+    klass_bit = unit.to_s.capitalize.gsub(/_[a-z]/) do |dash_letter| 
+      dash_letter[1].upcase
+    end
+    klass_name = "#{klass_bit}Calculator"
+    DateDiff.const_get klass_name
+  end
+  private :fetch_subclass
+
+  Calculator = Struct.new :from, :to do
+    def calculate
+      fail
+    end
+
+    def days_between
+      (to - from).to_f + 1
+    end
+    private :days_between
+  end
+
+  class DayCalculator < Calculator
+    def calculate
+      days_between
+    end
+  end
+
+  class WeekCalculator < DayCalculator
+    def calculate
+      super / 7.0
+    end
+  end
+
+  class ComplexCalculator < Calculator
+    attr :first_unit, :last_unit
+
+    def initialize *args
+      super
+      @first_unit = fetch_unit from
+      @last_unit  = fetch_unit to
+    end
+
+    def calculate
+      if first_unit.begin == last_unit.begin
+        days_between / days_in_unit(first_unit)
+      else
+        calculate_unit(from, first_unit.end) +
+          units_between +
+          calculate_unit(last_unit.begin, to)
+      end
+    end
+
+    private
+
+    def calculate_unit unit_begin, unit_end
+      self.class.new(unit_begin, unit_end).calculate
+    end
+
+    def calculate_units_between start, finish
+      count = 1
+      until start == finish
+        count += 1
+        start = advance_one_unit start
+      end
+      count
+    end
+
+    def days_in_unit unit
+      ((unit.end + 1) - unit.begin).to_f
+    end
+
+    def units_between
+      start = first_unit.end + 1
+      finish = rewind_one_unit last_unit.begin
+      return 0 if start > finish
+      calculate_units_between start, finish
+    end
+  end
+
+  class YearCalculator < ComplexCalculator
+    def fetch_unit date
+      (Date.new(date.year)..Date.new(date.year, 12, 31))
+    end
+
+    def advance_one_unit date
+      date.next_year
+    end
+
+    def rewind_one_unit date
+      date.prev_year
+    end
+  end
+
+  class QuarterCalculator < ComplexCalculator
+    def fetch_unit date
+      [1, 4, 7, 10].each do |quarter|
+        if (quarter..quarter + 2).include? date.month
+          start_of_quarter = Date.new(date.year, quarter)
+          return (start_of_quarter..(start_of_quarter.next_month.next_month.next_month - 1))
+        end
+      end
+      fail "Date month was #{date.month}"
+    end
+
+    def advance_one_unit date
+      date.next_month.next_month.next_month
+    end
+
+    def rewind_one_unit date
+      date.prev_month.prev_month.prev_month
+    end
+  end
+
+  class MonthCalculator < ComplexCalculator
+    def fetch_unit date
+      start_of_month = Date.new(date.year, date.month)
+      (start_of_month..(start_of_month.next_month - 1))
+    end
+
+    def advance_one_unit date
+      date.next_month
+    end
+
+    def rewind_one_unit date
+      date.prev_month
+    end
+  end
+
+end
--- test/models/date_diff_test.rb
+++ test/models/date_diff_test.rb
@@ -1,8 +1,6 @@
 require 'test_helper'
 
 class DateDiffTest < MiniTest::Unit::TestCase
-  include DateDiff
-
   def setup
     @jan_1_2000  = Date.new 2000, 1, 1
     @jan_31_2000 = Date.new 2000, 1, 31
@@ -13,73 +11,86 @@ class DateDiffTest < MiniTest::Unit::TestCase
     @jan_2_2001  = Date.new 2001, 1, 2
   end
 
-  def test_date_diff_method_invokes_appropriate_diff
-    assert_equal (1.0 / 366.0), date_diff(:year, @jan_1_2000, @jan_1_2000)
-    assert_equal (1.0 / 31.0), date_diff(:month, @jan_1_2000, @jan_1_2000)
-    assert_equal (1.0 / 7.0), date_diff(:week, @jan_1_2000, @jan_1_2000)
-    assert_equal 1.0, date_diff(:day, @jan_1_2000, @jan_1_2000)
+  def test_date_diff_via_keyword_args
+    via_keyword_args = date_diff(
+      unit: :year,
+      from: @jan_1_2000,
+      to: @jan_1_2000,
+    )
+    via_positional_params = date_diff(
+      :year,
+      @jan_1_2000,
+      @jan_1_2000,
+    )
+    assert_equal via_positional_params, via_keyword_args
   end
 
   def test_years_between_a_few_days
-    assert_equal (1.0   / 366.0), diff_years(@jan_1_2000, @jan_1_2000)
-    assert_equal (31.0  / 366.0), diff_years(@jan_1_2000, @jan_31_2000)
-    assert_equal (32.0  / 366.0), diff_years(@jan_1_2000, @feb_1_2000)
-    assert_equal (2/366.0 + 2/365.0), diff_years(@dec_30_2000, @jan_2_2001)
+    assert_equal (1.0   / 366.0), date_diff(:year, @jan_1_2000, @jan_1_2000)
+    assert_equal (31.0  / 366.0), date_diff(:year, @jan_1_2000, @jan_31_2000)
+    assert_equal (32.0  / 366.0), date_diff(:year, @jan_1_2000, @feb_1_2000)
+    assert_equal (2/366.0 + 2/365.0), date_diff(:year, @dec_30_2000, @jan_2_2001)
   end
 
   def test_quarters_between_a_few_days
     q1_2000 = (31 + 29 + 31).to_f
     q4_2000 = (31 + 30 + 31).to_f
     q1_2001 = (31 + 28 + 31).to_f
-    assert_equal (1.0  / q1_2000), diff_quarters(@jan_1_2000, @jan_1_2000)
-    assert_equal (31.0 / q1_2000), diff_quarters(@jan_1_2000, @jan_31_2000)
-    assert_equal (32.0 / q1_2000), diff_quarters(@jan_1_2000, @feb_1_2000)
-    assert_equal (2/q4_2000 + 2/q1_2001), diff_quarters(@dec_30_2000, @jan_2_2001)
+    assert_equal (1.0  / q1_2000), date_diff(:quarter, @jan_1_2000, @jan_1_2000)
+    assert_equal (31.0 / q1_2000), date_diff(:quarter, @jan_1_2000, @jan_31_2000)
+    assert_equal (32.0 / q1_2000), date_diff(:quarter, @jan_1_2000, @feb_1_2000)
+    assert_equal (2/q4_2000 + 2/q1_2001), date_diff(:quarter, @dec_30_2000, @jan_2_2001)
   end
 
   def test_months_between_a_few_days
-    assert_equal (1.0   / 31.0), diff_months(@jan_1_2000, @jan_1_2000)
-    assert_equal (31.0  / 31.0), diff_months(@jan_1_2000, @jan_31_2000)
-    assert_equal (1.0 + 1/29.0), diff_months(@jan_1_2000, @feb_1_2000)
-    assert_equal (2.0/31.0 + 2.0/31.0), diff_months(@dec_30_2000, @jan_2_2001)
+    assert_equal (1.0   / 31.0), date_diff(:month, @jan_1_2000, @jan_1_2000)
+    assert_equal (31.0  / 31.0), date_diff(:month, @jan_1_2000, @jan_31_2000)
+    assert_equal (1.0 + 1/29.0), date_diff(:month, @jan_1_2000, @feb_1_2000)
+    assert_equal (2.0/31.0 + 2.0/31.0), date_diff(:month, @dec_30_2000, @jan_2_2001)
   end
 
   def test_weeks_between_a_few_days
-    assert_equal (1.0   / 7.0), diff_weeks(@jan_1_2000, @jan_1_2000)
-    assert_equal (31.0  / 7.0), diff_weeks(@jan_1_2000, @jan_31_2000)
-    assert_equal (32.0  / 7.0), diff_weeks(@jan_1_2000, @feb_1_2000)
-    assert_equal (2/7.0 + 2/7.0), diff_weeks(@dec_30_2000, @jan_2_2001)
+    assert_equal (1.0   / 7.0), date_diff(:week, @jan_1_2000, @jan_1_2000)
+    assert_equal (31.0  / 7.0), date_diff(:week, @jan_1_2000, @jan_31_2000)
+    assert_equal (32.0  / 7.0), date_diff(:week, @jan_1_2000, @feb_1_2000)
+    assert_equal (2/7.0 + 2/7.0), date_diff(:week, @dec_30_2000, @jan_2_2001)
   end
 
   def test_days_between_a_few_days
-    assert_equal 1.0, diff_days(@jan_1_2000, @jan_1_2000)
-    assert_equal 31.0, diff_days(@jan_1_2000, @jan_31_2000)
-    assert_equal 32.0, diff_days(@jan_1_2000, @feb_1_2000)
-    assert_equal 4.0, diff_days(@dec_30_2000, @jan_2_2001)
+    assert_equal 1.0, date_diff(:day, @jan_1_2000, @jan_1_2000)
+    assert_equal 31.0, date_diff(:day, @jan_1_2000, @jan_31_2000)
+    assert_equal 32.0, date_diff(:day, @jan_1_2000, @feb_1_2000)
+    assert_equal 4.0, date_diff(:day, @dec_30_2000, @jan_2_2001)
   end
 
   def test_six_months
-    assert_equal ((1 + 29 + 31 + 30 + 31 + 2) / 366.0), diff_years(@jan_31_2000, @jun_2_2000)
-    assert_equal (1.0/31.0 + 4.0 + 2.0/30.0), diff_months(@jan_31_2000, @jun_2_2000)
-    assert_equal ((1 + 29 + 31 + 30 + 31 + 2) / 7.0), diff_weeks(@jan_31_2000, @jun_2_2000)
-    assert_equal (1 + 29 + 31 + 30 + 31 + 2), diff_days(@jan_31_2000, @jun_2_2000)
+    assert_equal ((1 + 29 + 31 + 30 + 31 + 2) / 366.0), date_diff(:year, @jan_31_2000, @jun_2_2000)
+    assert_equal (1.0/31.0 + 4.0 + 2.0/30.0), date_diff(:month, @jan_31_2000, @jun_2_2000)
+    assert_equal ((1 + 29 + 31 + 30 + 31 + 2) / 7.0), date_diff(:week, @jan_31_2000, @jun_2_2000)
+    assert_equal (1 + 29 + 31 + 30 + 31 + 2), date_diff(:day, @jan_31_2000, @jun_2_2000)
   end
 
   def test_a_single_year
-    assert_equal 1.0, diff_years(@jan_1_2000, @dec_31_2000)
-    assert_equal 4.0, diff_quarters(@jan_1_2000, @dec_31_2000)
-    assert_equal 12.0, diff_months(@jan_1_2000, @dec_31_2000)
+    assert_equal 1.0, date_diff(:year, @jan_1_2000, @dec_31_2000)
+    assert_equal 4.0, date_diff(:quarter, @jan_1_2000, @dec_31_2000)
+    assert_equal 12.0, date_diff(:month, @jan_1_2000, @dec_31_2000)
   end
 
   def test_full_years
     dec_12_2004 = Date.new 2004, 12, 31
-    assert_equal 5.0, diff_years(@jan_1_2000, dec_12_2004)
-    assert_equal 20.0, diff_quarters(@jan_1_2000, dec_12_2004)
-    assert_equal 60.0, diff_months(@jan_1_2000, dec_12_2004)
+    assert_equal 5.0, date_diff(:year, @jan_1_2000, dec_12_2004)
+    assert_equal 20.0, date_diff(:quarter, @jan_1_2000, dec_12_2004)
+    assert_equal 60.0, date_diff(:month, @jan_1_2000, dec_12_2004)
   end
 
   def test_a_year_and_four_days_across_a_leap_year
-    assert_equal (2/366.0 + 1.0 + 2/365.0), diff_years(@dec_30_2000, Date.new(2002, 1, 2))
-    assert_equal (2/31.0 + 12.0 + 2/31.0), diff_months(@dec_30_2000, Date.new(2002, 1, 2))
+    assert_equal (2/366.0 + 1.0 + 2/365.0), date_diff(:year, @dec_30_2000, Date.new(2002, 1, 2))
+    assert_equal (2/31.0 + 12.0 + 2/31.0), date_diff(:month, @dec_30_2000, Date.new(2002, 1, 2))
+  end
+
+  private
+
+  def date_diff *args
+    DateDiff.date_diff *args
   end
 end
