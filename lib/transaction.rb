773f14d0329394eedc0c274df2522992afa5e262
ntl <nathanladd+github@gmail.com>
2013-09-22 16:35:16 -0500

    Refactor how recurring transactions are calculated

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,80 +1,125 @@
 class Transaction
-  attr :credits, :date, :debits, :recurring_schedule
+  attr :credits, :debits, :schedule
 
-  RecurringSchedule = Struct.new :number, :unit, :end do
-    def initialize *args
-      super
+  class OneTimeSchedule
+    attr :date
+
+    def initialize date
+      @date = date
       freeze
+    end
+
+    def after? other_date
+      date > other_date
+    end
+
+    def apply! transaction, range, &block
+      yield_over_each_bit transaction.credits, transaction.debits, &block
+      nil
+    end
+
+    def before? other_date
+      date < other_date
+    end
+
+    def recurring?
+      false
+    end
+
+    private
+
+    def yield_over_each_bit credits, debits, multiplier = 1
+      credits.each do |amount, account_id|
+        yield :credit, amount * multiplier, account_id
+      end
+      debits.each do |amount, account_id|
+        yield :debit, amount * multiplier, account_id
+      end
+    end
+  end
+
+  class RecurringSchedule < OneTimeSchedule
+    attr :end, :number, :unit
+      
+    def initialize date, number, unit, _end
+      @end    = _end
+      @number = number
+      @unit   = unit
+      super date
+    end
+
+    def after? other_date
+      date > other_date
+    end
+
+    def apply! transaction, range, &block
+      yield_over_each_bit(
+        transaction.credits,
+        transaction.debits,
+        calculate_range_multiplier(range),
+        &block
+      )
+      remainder_transaction transaction, range
+    end
+
+    def before? other_date
+      date < other_date
+    end
+
+    def recurring?
+      true
+    end
+
+    private
+
+    def calculate_range_multiplier range
+      txn_start = [range.begin, date].max
+      txn_end   = [range.end, self.end].min
+      DateDiff.date_diff(unit: unit, from: txn_start, to: txn_end) * (1.0 / number)
+    end
+
+    def remainder_transaction transaction, range
+      return nil if self.end <= range.end
+      Transaction.new(
+        date:    range.end + 1,
+        credits: transaction.credits,
+        debits:  transaction.debits,
+        recurring_schedule: [number, unit, self.end],
+      )
     end
   end
 
   def initialize params = {}
-    @date = params.fetch :date, Projector::ABSOLUTE_START
+    date = params.fetch :date
     @credits = Array params[:credits]
     @debits  = Array params[:debits]
     @credits << params[:credit] if params[:credit]
     @debits  << params[:debit]  if params[:debit]
     if params[:recurring_schedule]
-      @recurring_schedule = build_recurring_schedule *params[:recurring_schedule]
+      @schedule = build_recurring_schedule date, *params[:recurring_schedule]
+    else
+      @schedule = OneTimeSchedule.new date
     end
     freeze
   end
 
-  def after? date
-    date > date
-  end
-
-  def closes_in_range? range
-    if recurring_schedule
-      recurring_schedule.end < range.end
-    else
-      date < range.end
-    end
-  end
-
   def apply! projector, &block
     validate! projector
-    range = projector.range
-    if date > range.end
-      return self
-    elsif recurring_schedule
-      txn_start = [range.begin, date].max
-      txn_end   = [range.end, recurring_schedule.end].min
-      range_multiplier = [
-        DateDiff.date_diff(
-          unit: recurring_schedule.unit,
-          from: txn_start,
-          to:   txn_end,
-        ),
-        (1.0 / recurring_schedule.number),
-      ].inject &:*
-      each_bit range_multiplier, &block
-      if recurring_schedule.end <= range.end
-        nil
-      else
-        new_start = range.end + 1
-        new_recurring_schedule = [
-          recurring_schedule.number,
-          recurring_schedule.unit,
-          recurring_schedule.end,
-        ]
-        self.class.new(
-          date:    new_start,
-          credits: credits,
-          debits:  debits,
-          recurring_schedule: new_recurring_schedule,
-        )
-      end
+    if schedule.after? projector.range.end
+      self
     else
-      each_bit &block
-      nil
+      schedule.apply! self, projector.range, &block
     end
+  end
+
+  def recurring_schedule
+    schedule
   end
 
   def validate! projector
-    if projector.from > date
+    if schedule.before? projector.from
       raise Projector::InvalidTransaction, "Transactions cannot occur before "\
-        "projection start date. (#{projector.from} vs. #{date})"
+        "projection start date. (#{projector.from} vs. #{schedule.date})"
     end
     total_credits, total_debits = total_credits_and_debits
     unless total_credits == total_debits
@@ -84,17 +129,8 @@ class Transaction
 
   private
 
-  def build_recurring_schedule number, unit, to = Projector::ABSOLUTE_END
-    RecurringSchedule.new number, unit, to
-  end
-
-  def each_bit multiplier = 1
-    credits.each do |amount, account_id|
-      yield :credit, amount * multiplier, account_id
-    end
-    debits.each do |amount, account_id|
-      yield :debit, amount * multiplier, account_id
-    end
+  def build_recurring_schedule date, number, unit, to = Projector::ABSOLUTE_END
+    RecurringSchedule.new date, number, unit, to
   end
 
   def total_credits_and_debits
