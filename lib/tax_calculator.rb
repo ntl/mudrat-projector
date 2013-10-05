be3bddf761d916280c89bb92dae14a651a3ba859
ntl <nathanladd+github@gmail.com>
2013-10-05 12:08:40 -0500

    Massive improvements to tax calculator

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -31,8 +31,9 @@ class TaxCalculator
     end
   end
 
-  def deduction
-    values_hash.fetch :standard_deduction
+  def deduction itemized_deduction
+    standard = values_hash.fetch :standard_deduction
+    [standard, itemized_deduction].max
   end
 
   def exemption
@@ -43,31 +44,92 @@ class TaxCalculator
     values_hash.fetch :standard_deduction
   end
 
-  def medicare_ss
-    (values_hash.fetch(:oasdi) + values_hash.fetch(:hi)) / 100
+  def employee_medicare_ss
+    value = employer_medicare_ss
+    value -= 0.02 if [2011, 2012].include? year
+    value
+  end
+
+  def employer_medicare_ss
+    medicare = values_hash.fetch :hi
+    ss       = values_hash.fetch :oasdi
+    (medicare + ss) / 100.0
+  end
+
+  def self_employment_tax
+    employee_medicare_ss + employer_medicare_ss
+  end
+
+  def calculate_se_tax gross
+    gross * (1 - employer_medicare_ss) * self_employment_tax
+  end
+
+  def calculate_w2_medicare_ss gross
+    wage_base     = values_hash.fetch :oasdi_wage_base
+    medicare_only = values_hash.fetch(:hi) / 100
+
+    under_wage_base = [gross, wage_base].min
+    over_wage_base  = [gross - wage_base, 0].max
+
+    (under_wage_base * employee_medicare_ss) + (over_wage_base * medicare_only)
+  end
+
+  def hsa_deductions hash
+    hash.reduce [0, 0] do |(atl, pretax), (_, account)|
+      atl_amount = account.fetch :amount
+      pre_amount = account.fetch(:pretax) ? atl_amount : 0
+      [atl + atl_amount, pretax + pre_amount]
+    end
+  end
+
+  def hsa_cap account
+    hsa_limits = values_hash.fetch :hsa_limit
+    cap =
+      if account.tag? :individual
+        hsa_limits.fetch :individual
+      elsif account.tag? :family
+        hsa_limits.fetch :family
+      else
+        fail "hsa accounts need a family or individual tag"
+      end
+    cap += hsa_limits.fetch(:senior) if account.tag?(:senior)
+    cap
   end
 
   def project
     se_gross = 0
     w2_gross = 0
+    pretax   = 0
+    hsa      = {}
+    itemized = 0
 
     @projection = projector.project to: Date.new(year, 12, 31) do |account, amount|
-      if account.type == :revenue
-        if account.tag? :self_employed
+      if account.tag? :self_employed
+        if account.type == :revenue
           se_gross += amount
-        elsif account.tag? :w2
-          w2_gross += amount
+        elsif account.type == :expense
+          se_gross -= amount
         end
-      elsif account.type == :expense && account.tag?(:business)
-        se_gross -= amount
+      elsif account.tag?(:w2) && account.type == :revenue
+        w2_gross += amount
+      elsif account.tag?(:hsa) && account.type == :asset
+        cap = hsa_cap account
+        hsa[account.id] ||= { amount: 0, pretax: account.tag?(:pretax) }
+        hsa[account.id][:amount] = [hsa[account.id][:amount] + amount, cap].min
+      elsif account.tag?(:'501c') && account.type == :expense
+        itemized += amount
       end
     end
 
-    se_tax = se_gross * (1 - medicare_ss) * (medicare_ss * 2)
-
     gross = se_gross + w2_gross
-    agi = gross - ((se_tax / 2) + deduction + exemption)
+    atl, pretax = hsa_deductions hsa
+
+    se_tax         = calculate_se_tax se_gross
+    w2_medicare_ss = calculate_w2_medicare_ss(w2_gross - pretax)
+
+    agi = gross - atl - ((se_tax / 2) + deduction(itemized) + exemption)
     taxes = calculate_income_tax agi
+    taxes += w2_medicare_ss
     taxes += se_tax
 
     OpenStruct.new(
@@ -88,7 +150,7 @@ class TaxCalculator
     parsed = YAML.load(File.read(yaml)).fetch(year).tap do |hash|
       recursively_symbolize_keys! hash
     end
-    hash = parsed.fetch :single
+    hash = parsed.fetch household.filing_status
     parsed.each_with_object hash do |(key, value), hash|
       hash[key] = value unless HOUSEHOLD_TYPES.include? key
     end
--- test/models/tax_calculator_test.rb
+++ test/models/tax_calculator_test.rb
@@ -1,42 +1,191 @@
 require 'test_helper'
 
 class TaxCalculatorShelleyTest < Minitest::Unit::TestCase
-  # http://taxes.about.com/od/paymentoptions/a/estimated_tax_3.htm
-  def test_shelley
-    projector = Projector.new from: jan_1_2013
-    projector.accounts = {
-      checking:     { type: :asset,   opening_balance: 50000 },
-      biz_expenses: { type: :expense, tags: %i(business)},
-      job:          { type: :revenue,
+  def setup
+    @projector = Projector.new from: jan_1_2013
+    @projector.accounts = {
+      checking:     { type: :asset,    opening_balance: 50000 },
+      w2_job:       { type: :revenue,  tags: %i(w2)},
+      charity:      { type: :expense,  tags: %i(501c)},
+      hsa:          { type: :asset,    tags: %i(hsa individual)},
+      se_expenses:  { type: :expense,  tags: %i(self_employed)},
+      se_job:       { type: :revenue,
                       opening_balance: 50000,
                       tags: %i(self_employed)},
     }
-    projector.transactions = [{
+  end
+
+  # http://taxes.about.com/od/paymentoptions/a/estimated_tax_3.htm
+  def test_shelley
+    @projector.transactions = [{
       date:     jan_1_2013,
-      credit:   [25000 / 3.0, :job],
+      credit:   [25000 / 3.0, :se_job],
       debit:    [25000 / 3.0, :checking],
       schedule: every_month(dec_31_2013),
     },{
       date:     jan_1_2013,
       credit:   [7500 / 3.0, :checking],
-      debit:    [7500 / 3.0, :biz_expenses],
+      debit:    [7500 / 3.0, :se_expenses],
       schedule: every_month(dec_31_2013),
     }]
 
-    household = TaxCalculator::Household.new :single, 1
+    tax_calculation = calculate single_household
+    assert_equal 2013, tax_calculation.year
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 19583
+  end
+
+  def test_shelly_2012
+    @projector.instance_variable_set :@from, jan_1_2012
+
+    @projector.transactions = [{
+      date:     jan_1_2012,
+      credit:   [17500 / 3.0, :se_job],
+      debit:    [17500 / 3.0, :checking],
+      schedule: every_month(dec_31_2012),
+    }]
+
+    tax_calculation = calculate single_household
+    assert_equal 2012, tax_calculation.year
+    # shoudl be lower, see http://en.wikipedia.org/wiki/Tax_Relief,_Unemployment_Insurance_Reauthorization,_and_Job_Creation_Act_of_2010
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 18514
+  end
+
+  def test_shelley_as_salaried_without_workplace_deductions
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [17500 / 3.0, :w2_job],
+      debit:    [17500 / 3.0, :checking],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate single_household
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 16284
+  end
+
+  def test_married_filing_jointly_with_two_kids
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [17500 / 3.0, :w2_job],
+      debit:    [17500 / 3.0, :checking],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 10793
+  end
+
+  def test_social_security_wage_base
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [10000, :w2_job],
+      debit:    [10000, :checking],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 120000, taxes: 23696.9
+  end
+
+  def test_mix_of_salaried_and_self_employed
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [10000, :w2_job],
+      debit:    [10000, :checking],
+      schedule: every_month(dec_31_2013),
+    },{
+      date:     jan_1_2013,
+      credit:   [800, :se_job],
+      debit:    [800, :checking],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 129600, taxes: 27284.10
+  end
+
+  def test_itemized_deductions
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [17500 / 3.0, :w2_job],
+      debit:    [17500 / 3.0, :checking],
+      schedule: every_month(dec_31_2013),
+    },{
+      date:     jan_1_2013,
+      credit:   [1500, :checking],
+      debit:    [1500, :charity],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 9922.5
+  end
+
+  def test_hsa_above_the_line_deduction
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [17500 / 3.0, :w2_job],
+      debit:    [17500 / 3.0, :checking],
+      schedule: every_month(dec_31_2013),
+    },{
+      date:     jan_1_2013,
+      credit:   [300, :checking],
+      debit:    [300, :hsa],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 70000, taxes: 10305
+  end
+
+  def test_hsa_pretax_deduction
+    @projector.accounts[:hsa].instance_variable_set :@tags, %i(hsa family senior)
+    @projector.transactions = [{
+      date:     jan_1_2013,
+      credit:   [10000, :w2_job],
+      debits:   [[9000, :checking],
+                 [1000, :hsa]],
+      schedule: every_month(dec_31_2013),
+    }]
+
+    tax_calculation = calculate married_two_kids_household
+    assert_tax_calculation tax_calculation, gross: 120000, taxes: 21655
+  end
+
+  def test_alternative_minimum
+    skip
+  end
+  
+  private
+
+  def calculate household
     tax_calculator = TaxCalculator.new(
       household: household,
-      projector: projector,
+      projector: @projector,
     )
-    tax_calculation = tax_calculator.project
+    tax_calculator.project
+  end
 
-    assert_equal 2013,  tax_calculation.year
-    assert_equal 70000, tax_calculation.gross
-    assert_equal 19583, tax_calculation.taxes.round
-    assert_equal 50417, tax_calculation.net.round
-    assert_equal 27.98, tax_calculation.effective_rate
+  def married_two_kids_household
+    TaxCalculator::Household.new :married_filing_jointly, 4
+  end
+
+  def single_household
+    TaxCalculator::Household.new :single, 1
+  end
+
+  def assert_tax_calculation tax_calculation, gross: 0, taxes: 0
+    expected_net  = gross - taxes
+    expected_rate = ((taxes * 100.0) / gross).round 2
+
+    assert_in_delta gross,         tax_calculation.gross,          1
+    assert_in_delta taxes,         tax_calculation.taxes,          1
+    assert_in_delta expected_net,  tax_calculation.net,            1
+    assert_in_delta expected_rate, tax_calculation.effective_rate, 0.01
   end
 end
+
+
+__END__
 
 class TaxCalculatorTest < Minitest::Unit::TestCase
   def setup
