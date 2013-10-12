634db79c8ae1b63636c38002ccba21e27e7a3785
ntl <nathanladd+github@gmail.com>
2013-10-12 06:39:47 -0500

    Refactor ScheduledTransaction

--- lib/scheduled_transaction.rb
+++ lib/scheduled_transaction.rb
@@ -2,33 +2,25 @@ class ScheduledTransaction < Transaction
   attr :schedule
 
   def initialize params = {}
-    @schedule = build_schedule params.fetch :schedule
+    @schedule = Schedule.new params.fetch :schedule
     super params
   end
 
   def slice slice_date
     txns_before_date = []
-    next_schedule = schedule.slice(date..slice_date) do |date, prorate|
-      txns_before_date << Transaction.new(
-        date: date,
-        debits: debits,
-        credits: credits,
-      )
+    next_schedule = schedule.slice(date..slice_date) do |new_date, prorate|
+      txns_before_date.push clone_transaction new_date: new_date
     end
     if next_schedule
-      leftover = ScheduledTransaction.new(
-        date: slice_date + 1,
-        debits: debits,
-        credits: credits,
-        schedule: next_schedule,
-      )
-    else
-      leftover = nil
+      leftover = clone_transaction new_date: slice_date + 1, new_schedule: next_schedule
     end
     [txns_before_date, leftover]
   end
 
-  def build_schedule schedule_params
-    Schedule.new schedule_params
+  def clone_transaction new_date: date, new_schedule: nil
+    params = { debits: debits, credits: credits, date: new_date }
+    return Transaction.new(params) if new_schedule.nil?
+    params[:schedule] = new_schedule
+    ScheduledTransaction.new params
   end
 end
